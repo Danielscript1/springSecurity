9c351b65fe40f1efe733e3e908113784d0a4cf88
Danielscript1 <danielsousagit@gmail.com>
2021-12-08 15:00:26 -0300

    usandoJDBCAuthetication

--- 1530-springmvc-parte1-CodigoInicialCursoParte2/src/main/java/br/com/alura/mvc/mudi/WebSecurityConfig.java
+++ 1530-springmvc-parte1-CodigoInicialCursoParte2/src/main/java/br/com/alura/mvc/mudi/WebSecurityConfig.java
@@ -1,20 +1,25 @@
 package br.com.alura.mvc.mudi;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
+	@Autowired
+	private javax.sql.DataSource dataSource;
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
@@ -26,17 +31,27 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
             .permitAll()
         )
 		.logout(logout -> logout.logoutUrl("/logout"));}
-	
-	@Bean
+	//autenticação com banco
 	@Override
-	public UserDetailsService userDetailsService() {
-		UserDetails user =
-			 User.withDefaultPasswordEncoder()
-				.username("daniel")
-				.password("daniel")
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+	BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+	
+	UserDetails user =
+			 User.builder()
+				.username("david")
+				.password(encoder.encode("david"))
 				.roles("ADM")
 				.build();
-
-		return new InMemoryUserDetailsManager(user);
+	
+	
+	auth.jdbcAuthentication()
+	  .dataSource(dataSource)
+	  .passwordEncoder(encoder)
+	  .withUser(user);
+	
+	
+		
 	}
+	
+	
 }
