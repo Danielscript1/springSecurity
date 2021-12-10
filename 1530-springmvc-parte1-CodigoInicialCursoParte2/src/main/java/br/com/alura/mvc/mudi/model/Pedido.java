6e620b5ae42c38199660727a61eb0a7e70694b28
Danielscript1 <danielsousagit@gmail.com>
2021-12-10 12:56:31 -0300

    CriandoRestController

--- 1530-springmvc-parte1-CodigoInicialCursoParte2/src/main/java/br/com/alura/mvc/mudi/model/Pedido.java
+++ 1530-springmvc-parte1-CodigoInicialCursoParte2/src/main/java/br/com/alura/mvc/mudi/model/Pedido.java
@@ -12,6 +12,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 @Entity
 public class Pedido {
 
@@ -24,7 +26,7 @@ public class Pedido {
 	private String urlProduto;
 	private String urlImagem;
 	private String descricao;
-	
+	@JsonIgnore
 	@ManyToOne(fetch= FetchType.LAZY)
 	private User user;
 	
