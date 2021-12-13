49dc648eeebdc489c70b42bd75cacbd1d099a91c
Danielscript1 <danielsousagit@gmail.com>
2021-12-13 15:08:16 -0300

    adicionandoExpressaoRegulares

--- 1530-springmvc-parte1-CodigoInicialCursoParte2/src/main/java/br/com/alura/mvc/mudi/dto/RequisicaoNovaOferta.java
+++ 1530-springmvc-parte1-CodigoInicialCursoParte2/src/main/java/br/com/alura/mvc/mudi/dto/RequisicaoNovaOferta.java
@@ -5,6 +5,9 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 import javax.persistence.Entity;
+import javax.validation.constraints.Pattern;
+
+import com.sun.istack.NotNull;
 
 import br.com.alura.mvc.mudi.model.Oferta;
 
@@ -13,7 +16,11 @@ public class RequisicaoNovaOferta {
 	private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
 	private Long pedidoId;
+	@Pattern(regexp = "^\\d+(\\.\\d+{2})?$")
+	@NotNull
 	private String valor;
+	@Pattern(regexp = "^\\d{2}/\\d{2}/\\d{4}$")
+	@NotNull
 	private String dataDaEntrega;
 	private String comentario;
 	public Long getPedidoId() {
